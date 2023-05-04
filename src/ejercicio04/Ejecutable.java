9cfedb44eabcd1377b2013e90e896575fdca86e3
15Carmen <carmen.martin@iesnervion.es>
2023-05-04 19:10:55 +0200

    no mucho pero algo

--- src/ejercicio04/Ejecutable.java
+++ src/ejercicio04/Ejecutable.java
@@ -11,8 +11,7 @@ public class Ejecutable {
      */
     public static void main(String[] args) {
 
-        //Creamos un array de 10 electrodomesticos
-        Electrodomestico[] arrayElec = new Electrodomestico[10];
+
 
         //Creamos los objetos de las clases Electrodomestico, Lavadora y Television
         Electrodomestico e1 = new Electrodomestico();
@@ -26,21 +25,12 @@ public class Ejecutable {
         Electrodomestico t3 = new Television(300, 60, 70, false);
 
        //Introducimos los objetos que acabamos de crear en el array
-        arrayElec[0] = e1;
-        arrayElec[1] = e2;
-        arrayElec[2] = e3;
-        arrayElec[3] = l1;
-        arrayElec[4] = l2;
-        arrayElec[5] = l3;
-        arrayElec[6] = t1;
-        arrayElec[7] = t2;
-        arrayElec[8] = t3;
+        //Creamos un array de 10 electrodomesticos
+        Electrodomestico[] arrayElec = {e1, e2, e3, l1, l2, l3, t1, t2, t3};
 
         //Recorremos el array y lo mostramos por pantalla
         for (Electrodomestico elec: arrayElec) {
-           if(elec != null){    //Siempre y cuando laa celda del array no sea nula
-               System.out.println(elec);
-           }
+            System.out.println(elec);
         }
 
 
